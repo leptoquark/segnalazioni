182ea907a6099cf7beabbbe3b6bcbff74a0a0b4a
Claudio Biancalana <c.biancalana@anticorruzione.it>
2021-09-10 11:30:10 +0200

    aggiornamento timer

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
+import { Observable, Subscription, timer } from 'rxjs';
 import { AusaRepository } from '../model/ausa.repository';
 import { RestDataSource } from '../model/rest.datasource';
 import { Submission } from '../model/submission.model';
@@ -46,13 +48,37 @@ export class FormComponent implements OnInit {
   
   refreshForm = new EventEmitter();
   form: any;
-
+  
   ngOnInit(): void {
-
+   
   }
 
+  render(event: any) {
+    let submissionDraft = "";
+    let dataAux = localStorage.getItem("draft");
+    if (dataAux != null)
+      submissionDraft = JSON.parse(dataAux);
+
+    this.refreshForm.emit({
+      form: this.form,
+      submission: {
+        data: submissionDraft
+      }
+    });
+   
+  }
+
+
+  customEvent(event: any)
+  {
+    /* azione per il salvataggio in bozza, nella localstorage, della sottomissione */
+    if (event.type === 'salvabozza'){
+      localStorage.setItem("draft",JSON.stringify(event.data));
+    }
+  }
   onChange(event: any) { 
-      if (event.changed && event.changed.component.key === 'codiceAusa' && event.changed.value)  {
+
+       if (event.changed && event.changed.component.key === 'codiceAusa' && event.changed.value)  {
         if (event.changed.value.length >= 10){
           var submissionAux = event;
           
@@ -75,7 +101,16 @@ export class FormComponent implements OnInit {
     }
   }
   
-  constructor(public router:Router, public sub: Submission, private repository: AusaRepository){}
+  everySecond: Observable<number> = timer(0, 1000);
+  private subscription: Subscription;
+
+  constructor(public router:Router, public sub: Submission, private repository: AusaRepository){
+
+    this.subscription = this.everySecond.subscribe((seconds) => {
+      console.log("test");
+    })
+
+  }
 
   get submission() {
     return this.sub.getId();
