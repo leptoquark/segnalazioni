06a5b3de832f86dd968fd233f06826efb0a75236
Claudio Biancalana <c.biancalana@anticorruzione.it>
2021-09-10 12:42:58 +0200

    cors2

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.use("/api", router);
 app.use(history());
 app.use("/", express.static("./dist/segnalazioni"));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+ })
+
 app.listen(8080, 
     () => console.log("HTTP Server running on port 8080"));
 
@@ -33,22 +40,3 @@ if (enableHttps) {
 } else {
     console.log("HTTPS disabled")
 }
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://nodejs-mongodb-formio-segnalazioni-ril.apps.ocp.premaster.local');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
