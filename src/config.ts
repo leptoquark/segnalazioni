e74c01ccdec6f6a625b8c2a9c70024610fa2ec49
Claudio Biancalana <c.biancalana@anticorruzione.it>
2021-11-17 16:20:10 +0100

    'aggiornamnento'

--- src/config.ts
+++ src/config.ts
@@ -9,9 +9,9 @@ export const AppConfig: FormioAppConfig = {
 
 export const EnvConfig: any = {
    //appUrl: 'http://localhost:3001',
-   backendUrl: 'http://localhost:8080',
+   //backendUrl: 'http://localhost:8080',
    appUrl: 'http://nodejs-mongodb-formio-segnalazioni-ril.apps.ocp.premaster.local',
-   //backendUrl: 'http://segnalazioni-backend-segnalazioni-ril.apps.ocp.premaster.local',
+   backendUrl: 'http://segnalazioni-backend-segnalazioni-ril.apps.ocp.premaster.local',
    formId: 'segnalazione-dev',
    jwt_user: 'segnalazioni',
    jwt_password: 'segnalazionipasswordanac'
